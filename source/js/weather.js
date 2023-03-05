c291892c122e88830cb7c7098f9a1db5b86d3562
Ravanla <1285644869@qq.com>
2023-03-05 16:29:18 +0800

    first commit

---
+++ source/js/weather.js
@@ -0,0 +1,53 @@
+{/* <div id="he-plugin-simple"></div>
+<script> */}
+// WIDGET = { // 北京
+//   "CONFIG": {
+//     "modules": "01234",
+//     "background": "1",
+//     "tmpColor": "FFFFFF",
+//     "tmpSize": "18",
+//     "cityColor": "FFFFFF",
+//     "citySize": "18",
+//     "aqiColor": "FFFFFF",
+//     "aqiSize": "18",
+//     "weatherIconSize": "24",
+//     "alertIconSize": "16",
+//     "padding": "10px 15px 10px 15px",
+//     "shadow": "0",
+//     "language": "auto",
+//     "borderRadius": "10",
+//     "fixed": "false",
+//     "vertical": "top",
+//     "horizontal": "left",
+//     "key": "69c21d9044b04aaf981b304c151f64c0"
+//   }
+// }
+{/* <div id="he-plugin-simple"></div>
+<script> */}
+WIDGET = { // 深圳南山
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "18",
+    "cityColor": "FFFFFF",
+    "citySize": "18",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "18",
+    "weatherIconSize": "20",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "1",
+    "language": "auto",
+    "borderRadius": "15",
+    "fixed": "false",
+    "vertical": "center",
+    "horizontal": "center",
+    "city": "CN101280604",
+    "key": "ec636ae2d195490ca3fd894bde343c60"
+  }
+}
+{/* </script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script> */}
+{/* </script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script> */}
---
+++ source/life/music/index.md
@@ -0,0 +1,19 @@
+---
+title: 音乐馆
+date: 2021-04-24 21:41:30
+type: music
+aplayer: true
+top_img: false
+comments: false
+aside: false
+---
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/APlayer.min.css"> 
+<script src="https://cdn.jsdelivr.net/npm/aplayer@1.10/dist/APlayer.min.js"></script> 
+<script src="https://cdn.jsdelivr.net/npm/meting@1.2/dist/Meting.min.js"></script> 
+
+<!-- {% meting "7868005687" "tencent" "playlist" "theme:#555" "mutex:true" "listmaxheight:1000px" "preload:auto" %} -->
+<!-- https://i.y.qq.com/n2/m/share/details/taoge.html?id=8824475844&hosteuin= -->
+{% meting "8824855106" "tencent" "playlist" "theme:#555" "mutex:true" "listmaxheight:1000px" "preload:auto" %}
+
+<!-- D:\developer\ravanlaBlog\Ravanla.github.io\themes\butterfly\layout\includes\page\music.pug 配置链接-->
